85e037f5e1ee9be8514e7de5ddcd694855c47776
노트북 <asme12@naver.com>
2024-06-19 16:09:52 +0900

    CICD 테스트용 문자 추가

--- project/src/components/Hero.js
+++ project/src/components/Hero.js
@@ -36,7 +36,9 @@ const Hero = () => {
         {/* css와 디자인 부분 */}
         <p className="leading-relaxed tracking-wide">
           혼자만의 성장하는 방식이 아닌 상호간에 소통, 협력을 통한 성장을
-          중요시하는 개발자입니다.{" "}
+          중요시하는 개발자입니다.
+          잠시 CICD 테스트 용으로 추가된 문자입니다.
+          {" "}
         </p>
       </div>
     </section>
