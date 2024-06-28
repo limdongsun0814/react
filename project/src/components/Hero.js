f3c0b619a6cb9c031669104cb5daa2660b4f1b98
노트북 <asme12@naver.com>
2024-06-28 14:46:30 +0900

    소개 내용 수정

--- project/src/components/Hero.js
+++ project/src/components/Hero.js
@@ -27,16 +27,29 @@ const Hero = () => {
           <br></br>
           임동선입니다.
         </h1>
-        <span className="font-bold text-orange-200 ">“배움에 대한 욕심”</span>
+        <span className="font-bold text-orange-200 ">“사용자가 이용하고 싶은 서비스를 개발하는 개발자입니다.”</span>
         {/* 한컴독스 내용 */}
         <p className="leading-relaxed tracking-wide">
-          논어의 첫머리에 나오는 말처럼 배움과 성장에 욕심이 있는 개발자입니다.{" "}
+          모든 서비스에는 사용자가 존재하고, 이 사용자들이 쉽고 친숙하게 개발하는 개발자입니다. 
+          {" "}
         </p>
-        <span className="font-bold text-orange-200 ">“협력의 중요성”</span>
+        <p className="leading-relaxed tracking-wide">
+          대학원 시절에는 주로 기업 연계 사물 인터넷(IoT) 과제로 개발 했습니다. {" "}
+        </p>
+        <p className="leading-relaxed tracking-wide">
+          주로 담당 업무는 유선 통신(I2C, RS485 등), 제어(PID 제어), 필터(LPF, HPF, Kalman filter 등), 무선 통신(MQTT, HTML, LoRa, Zigbee 등)를 담당했습니다. 
+          {" "}
+        </p>
+        <span className="font-bold text-orange-200 ">“소통과 성장”</span>
         {/* css와 디자인 부분 */}
         <p className="leading-relaxed tracking-wide">
-          혼자만의 성장하는 방식이 아닌 상호간에 소통, 협력을 통한 성장을
-          중요시하는 개발자입니다.
+          자기 개발을 통해 획득한 성장을 상호 소통하여 공유하는 개발자입니다.
+        </p>
+        <p className="leading-relaxed tracking-wide">
+          사용자의 피드백과 다양한 방법의 매소드 구현으로 프로젝트의 퀄리티를 높이는 개발자입니다.
+        </p>
+        <p className="leading-relaxed tracking-wide">
+          주단위 계획하여 시간을 효율적으로 관리하는 개발자입니다.
           {" "}
         </p>
       </div>
